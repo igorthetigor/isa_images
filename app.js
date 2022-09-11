7709b7a05f7629901f28727cd078c00c7b603712
igorthetigor <igor.liverant@gmail.com>
2022-09-11 15:06:53 +0200

    shadow looks as expected

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const main = document.getElementsByClassName('Images');
+
+// main[0].addEventListener("wheel", (event) =>{
+//   console.log({ event });
+//   if(event.wheelDeltaY > 0) {
+//     console.log('scrolling up')
+//   } else {
+//     console.log('scrolling down')
+//   }
+//   if(event.shiftKey) {
+//     console.log('scrolling horizontally')
+//   }
+//   console.log({ mouseeventButtons: event.shiftKey });
+// })
