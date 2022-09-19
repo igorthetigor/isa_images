9e5b9eec0e6d1950cb0dadb0710c68d7224a1804
igorthetigor <igor.liverant@gmail.com>
2022-09-19 02:45:27 +0200

    images have fixed size no matter which screen. load only the amount of images which fit into viewport. define detailed breakpoints

---
+++ modules/helper-functions.js
@@ -0,0 +1,9 @@
+let latestImagesAmount;
+
+let WIDTH = window.innerWidth;
+
+function logAndReturn(valueOfInterest) {
+  return valueOfInterest;
+}
+
+export { latestImagesAmount, logAndReturn, WIDTH };
